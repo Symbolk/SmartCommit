8de6c82c70690968361d52f5b06d077d04a53a93
Symbolk <symbolk@163.com>
2019-06-18 19:34:02 +0800

    [fix] Use amd require for correctly packaging app with monaco-editor.

--- electron-app/diffview.js
+++ electron-app/diffview.js
@@ -1,53 +1,73 @@
-require.config({
-	paths: {
-		'vs': './node_modules/monaco-editor/min/vs'
-	}
-});
+// AMD require version to 
+(function () {
+    var nodeRequire = global.require
+    const path = require('path');
+    const amdLoader = require('./node_modules/monaco-editor/min/vs/loader.js');
+    var amdRequire = global.require
+    global.require = nodeRequire
+    amdRequire = amdLoader.require;
+    const amdDefine = amdLoader.require.define;
 
-require(['vs/editor/editor.main'], function () {
-	var diffEditor = monaco.editor.createDiffEditor(document.getElementById('monaco'), {
-		// theme: "vs-dark",
-		scrollBeyondLastLine: false
-	});
+    function uriFromPath(_path) {
+        var pathName = path.resolve(_path).replace(/\\/g, '/');
+        if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+            pathName = '/' + pathName;
+        }
+        return encodeURI('file://' + pathName);
+    }
 
-	monaco.Promise.join([xhr('changes.js'), xhr('diffview.js')]).then(function (r) {
-		var originalTxt = r[0].responseText;
-		var modifiedTxt = r[1].responseText;
+    amdRequire.config({
+        baseUrl: uriFromPath(path.join(__dirname, './node_modules/monaco-editor/min'))
+    });
 
-		diffEditor.setModel({
-			original: monaco.editor.createModel(originalTxt, 'javascript'),
-			modified: monaco.editor.createModel(modifiedTxt, 'javascript'),
-		})
-	});
-});
+    // workaround monaco-css not understanding the environment
+    self.module = undefined;
 
-function xhr(url) {
-	var req = null;
-	return new monaco.Promise(function (c, e, p) {
-		req = new XMLHttpRequest();
-		req.onreadystatechange = function () {
-			if (req._canceled) {
-				return;
-			}
+    amdRequire(['vs/editor/editor.main'], function () {
+        var diffEditor = monaco.editor.createDiffEditor(document.getElementById('monaco'), {
+            // theme: "vs-dark",
+            scrollBeyondLastLine: false
+        });
 
-			if (req.readyState === 4) {
-				if ((req.status >= 200 && req.status < 300) || req.status === 1223) {
-					c(req);
-				} else {
-					e(req);
-				}
-				req.onreadystatechange = function () {};
-			} else {
-				p(req);
-			}
-		};
+        monaco.Promise.join([xhr('changes.js'), xhr('diffview.js')]).then(function (r) {
+            var originalTxt = r[0].responseText;
+            var modifiedTxt = r[1].responseText;
 
-		req.open("GET", url, true);
-		req.responseType = "";
+            diffEditor.setModel({
+                original: monaco.editor.createModel(originalTxt, 'javascript'),
+                modified: monaco.editor.createModel(modifiedTxt, 'javascript'),
+            })
+        });
+    });
 
-		req.send(null);
-	}, function () {
-		req._canceled = true;
-		req.abort();
-	});
-}
+    function xhr(url) {
+        var req = null;
+        return new monaco.Promise(function (c, e, p) {
+            req = new XMLHttpRequest();
+            req.onreadystatechange = function () {
+                if (req._canceled) {
+                    return;
+                }
+
+                if (req.readyState === 4) {
+                    if ((req.status >= 200 && req.status < 300) || req.status === 1223) {
+                        c(req);
+                    } else {
+                        e(req);
+                    }
+                    req.onreadystatechange = function () {};
+                } else {
+                    p(req);
+                }
+            };
+
+            req.open("GET", url, true);
+            req.responseType = "";
+
+            req.send(null);
+        }, function () {
+            req._canceled = true;
+            req.abort();
+        });
+    }
+})();
