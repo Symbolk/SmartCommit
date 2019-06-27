e3c69c660ac93fad0e926126e4ef268a935b14d0
Symbolk <symbolk@163.com>
2019-06-27 20:07:42 +0800

    [fix] Config the devServer to avoid endless loading.

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -3,5 +3,12 @@ module.exports = {
         resolve: {
             symlinks: false
         }
-    }
+    },
+    devServer: {
+        // host:'0.0.0.0',
+        port: 8999
+    },
+    transpileDependencies: [
+        /\bvue-awesome\b/
+    ]
 }
