486519200bb769ea16628bf3bbc14bb9924c0410
Symbolk <symbolk@163.com>
2019-06-30 13:28:01 +0800

    [test] Testing the commit function.

--- vue-app/src/components/utils/test.js
+++ vue-app/src/components/utils/test.js
@@ -1,7 +1,18 @@
-const gitutils = require('./gitutils');
+/* eslint-disable no-console */
+// const gitutils = require('./gitutils');
+var git = require('simple-git');
 
-gitutils.getDiffFiles("").then(res => {
+git = git("")
+var commit_message = "Testing commit function."
+var file_list = ['src/components/utils/test.js']
+var options = {
+    // '--author': '"Symbolk <symbolk@outlook.com>"'
+}
+git.commit(commit_message, file_list, options, (err, res) => {
+    if (err) {
+        console.log(err);
+    }
     console.log(res);
-});
+})
 
-
+// git reset --soft HEAD^
