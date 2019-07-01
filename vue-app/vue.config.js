baec1d39ee4afdb85e004cb79db7ce8e6616a2ef
Symbolk <symbolk@163.com>
2019-07-01 20:40:43 +0800

    [build] Add build configurations for electron-builder.

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            symlinks: false
+  configureWebpack: {
+    resolve: {
+      symlinks: false
+    }
+  },
+  devServer: {
+    // host:'0.0.0.0',
+    port: 8999
+  },
+  transpileDependencies: [/\bvue-awesome\b/],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'edu.pku.smartcommit',
+        productName: 'SmartCommit',
+        copyright: 'Copyright © 2019 Bo Shen',
+        directories: {
+          buildResources: 'build'
         }
-    },
-    devServer: {
-        // host:'0.0.0.0',
-        port: 8999
-    },
-    transpileDependencies: [
-        /\bvue-awesome\b/
-    ]
+      }
+    }
+  }
 }
--- vue-app/webpack.config.js
+++ vue-app/webpack.config.js
@@ -1,21 +1,24 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 // const path = require('path')
 module.exports = {
-    // entry: './main.js',
-    // output: {
-    //   path: path.resolve(__dirname, 'dist'),
-    //   filename: 'app.js'
-    // },
-    // module: {
-    //   rules: [{
-    //     test: /\.css$/,
-    //     use: ['style-loader', 'css-loader']
-    //   }]
-    // },
-    plugins: [
-        new MonacoWebpackPlugin({
-            // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-            languages: ['javascript', 'css', 'html', 'typescript', 'json']
-        })
-    ]
-};
+  // entry: './main.js',
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'app.js'
+  // },
+  // module: {
+  //   rules: [{
+  //     test: /\.css$/,
+  //     use: ['style-loader', 'css-loader']
+  //   }]
+  // },
+  plugins: [
+    new MonacoWebpackPlugin({
+      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      languages: ['javascript', 'css', 'html', 'typescript', 'json']
+    })
+  ],
+  externals: {
+    'simple-git/promise': "require('simple-git/promise')"
+  }
+}
