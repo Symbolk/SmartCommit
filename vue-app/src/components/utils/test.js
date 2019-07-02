99b1fb6838b74297202ac4631019662025f997d6
Symbolk <symbolk@163.com>
2019-07-02 22:17:46 +0800

    [test] Test function to get root path of git repo.

--- vue-app/src/components/utils/test.js
+++ vue-app/src/components/utils/test.js
@@ -15,12 +15,47 @@ var options = {
 //     console.log(res);
 // })
 
-console.log(git._baseDir)
-git.status((err, res) => {
+const getRootPath = async repo_path => {
+  const gitP = require('simple-git/promise')
+
+  let topLevel = ''
+  try {
+    topLevel = await gitP(repo_path).revparse(['---show-toplevel'])
+  } catch (e) {
+    // handle the error
+  }
+  return topLevel
+}
+
+const getRootPath2 = repo_path => {
+  return new Promise((resolve, reject) => {
+    git.revparse(['--show-toplevel'], function(err, topLevel) {
+      if (err) {
+        reject(err)
+      }
+      resolve(topLevel)
+    })
+  })
+}
+console.log('1' + git._baseDir)
+git.revparse(['--show-toplevel'], function(err, topLevel) {
   if (err) {
     console.log(err)
   }
-  console.log(res)
+  console.log('2' + topLevel)
 })
+getRootPath('').then(rootPath => {
+  console.log('3' + rootPath)
+})
+getRootPath2('').then(rootPath => {
+  console.log('4' + rootPath)
+})
+
+// git.status((err, res) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.log(res)
+// })
 
 // git reset --soft HEAD^
