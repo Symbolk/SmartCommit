890197d1366c34e232f8349f3a86dfd375b6efab
Symbolk <symbolk@163.com>
2019-07-16 21:30:31 +0800

    [ui] Add local header image.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-carousel
-      :interval="4000"
+      :interval="0"
       @sliding-end="onSlideEnd"
       @sliding-start="onSlideStart"
       background="#ababab"
@@ -12,22 +12,19 @@
       style="text-shadow: 1px 1px 2px #333;"
       v-model="slide"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="Smart Commit"
-        img-src="https://picsum.photos/1024/200/?image=52"
-        text="An assistant to help you commit elegantly."
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/200/?image=54">
-        <h1>Hello world!</h1>
+      <!-- Main image -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/200/?image=24">
+        <h2>Smart Commit</h2>
+        <h4>An assistant to help you commit elegantly.</h4>
       </b-carousel-slide>
 
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/200/?image=58"></b-carousel-slide>
+      <!-- Slides with release notes -->
+      <b-carousel-slide
+        caption="Release Notes"
+        img-src="https://picsum.photos/1024/200/?image=24"
+      ></b-carousel-slide>
+        <!-- text="An assistant to help you commit elegantly." -->
 
-      <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
       <b-carousel-slide>
         <img
@@ -35,19 +32,13 @@
           class="d-block img-fluid w-100"
           height="200"
           slot="img"
-          src="https://picsum.photos/1024/200/?image=55"
+          src="https://picsum.photos/1024/200/?image=223"
           width="1024"
         />
       </b-carousel-slide>
 
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-alt="Blank image" img-blank>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
-    </b-carousel>
+    </b-carousel> 
+
     <a @click="openGithub()" class="github-corner" href>
       <svg
         height="100"
